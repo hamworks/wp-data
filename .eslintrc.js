9b71ca7c4baac285e79b701037deed6db2da5dc5
Hiroshi Urabe <urabe@ham.works>
2020-02-26 15:19:40 +0900

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,10 @@
+const defaultConfig = require( '@wordpress/scripts/config/.eslintrc.js' );
 module.exports = {
-	root: true,
-	"parser": "@typescript-eslint/parser",
+	...defaultConfig,
+	parser: "@typescript-eslint/parser",
 	extends: [
-		'plugin:@wordpress/eslint-plugin/recommended',
+		...defaultConfig.extends,
 		"plugin:@typescript-eslint/eslint-recommended",
 		"plugin:@typescript-eslint/recommended",
-		// Temporary workaround to use linting rules for both e2e and unit tests with all files
-		// until override files globbing logic is fixed when using with --config.
-		// @see https://github.com/eslint/eslint/issues/11558
-		'plugin:@wordpress/eslint-plugin/test-e2e',
-		'plugin:@wordpress/eslint-plugin/test-unit',
 	],
 };
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { useCallback } from '@wordpress/element';
 import { useDispatch, useSelect } from '@wordpress/data';
 
-export const useMeta = <T extends any>( key: string ): [ T, ( v: T ) => void ] => {
+export const useMeta = < T >( key: string ): [ T, ( v: T ) => void ] => {
 	const meta = useSelect( ( select ) => {
 		const { getEditedPostAttribute } = select( 'core/editor' );
-		const metas: { [ key: string ]: T } = getEditedPostAttribute( 'meta' ) || {};
+		const metas: { [ key: string ]: T } =
+			getEditedPostAttribute( 'meta' ) || {};
 		return metas[ key ];
 	}, [] );
 
@@ -17,4 +18,3 @@ export const useMeta = <T extends any>( key: string ): [ T, ( v: T ) => void ] =
 
 	return [ meta, setMeta ];
 };
-
